24e9654905c699ddec979d62a4f2bf71a4706f74
izourne <izourne003@gmail.com>
2024-09-02 02:58:14 -0700

    removed mongo service

--- src/config/config.js
+++ src/config/config.js
@@ -1,25 +1,25 @@
 // 3rd party libraries
 require("dotenv").config()
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 
 // env setup
-const env = process.env.NODE_ENV || "development"
-if (env === "testing") {
-  Object.assign(process.env, {
-    PORT: 5000,
-    DB_NAME: "books-test",
-    MONGODB_URI: "mongodb://root:example@tresmerge-mongo-1:27017/books-test",
-  })
-}
+// const env = process.env.NODE_ENV || "development"
+// if (env === "testing") {
+//   Object.assign(process.env, {
+//     PORT: 5000,
+//     DB_NAME: "books-test",
+//     MONGODB_URI: "mongodb://root:example@tresmerge-mongo-1:27017/books-test",
+//   })
+// }
 
 // database connection
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    authSource: "admin",
-  })
-  .then(() =>
-    console.log(`connected successfully to DB: ${process.env.DB_NAME}`)
-  )
-  .catch((error) => console.log(`failed to connect to DB: ${error}`))
+// mongoose
+//   .connect(process.env.MONGODB_URI, {
+//     authSource: "admin",
+//   })
+//   .then(() =>
+//     console.log(`connected successfully to DB: ${process.env.DB_NAME}`)
+//   )
+//   .catch((error) => console.log(`failed to connect to DB: ${error}`))
 
-module.exports = mongoose
+// module.exports = mongoose
